6aac3259ef22502b169f15f0d54247ac006a62d8
santhod <santhod11111@gmail.com>
2023-08-28 16:13:20 +0700

    f1

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,53 @@
 "use client";
-import { Container, Text, Title } from "@mantine/core";
+import {
+  Button,
+  Container,
+  Divider,
+  Group,
+  Pagination,
+  Rating,
+  Text,
+  Textarea,
+  Title,
+} from "@mantine/core";
 
 export default function FoodReviewPage() {
   return (
     <Container size="600px">
       <Title order={2}>Food Review 🍕</Title>
+      <Title order={4} mt="sm">
+        Your rating
+      </Title>
+      <Rating size="lg" />
+      <Textarea
+        label="Your review"
+        placeholder="Do you enjoy eating?"
+        mt="xs"
+        minRows={3}
+      />
+      <Button color="orange" mt="xs">
+        Submit Review
+      </Button>
+      <Divider my="sm" />
+      <Group position="center">
+        <Title order={4}>Elon Musk</Title>
+        <Rating size="sm" defaultValue={5} />
+      </Group>
+      <Text align="center" c="dimmed">
+        Best pizza in this world. I give you X score.
+      </Text>
+      <Divider my="sm" />
+      <Group position="center">
+        <Title order={4}>Mark Zuck</Title>
+        <Rating size="sm" defaultValue={4} />
+      </Group>
+      <Text align="center" c="dimmed">
+        My favourite part is pepperoni
+      </Text>
 
+      <Pagination mt="md" position="center" total={20} color="orange" />
       <Text align="center" color="dimmed" my="sm">
-        Copyright © 2023 Chayanin Suatap 650610560
+        Copyright © 2023 Santhod Khiawaubon 650612101
       </Text>
     </Container>
   );
